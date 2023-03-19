c317f1206027bcc2aa9aa82a0548c8ccf14a80c0
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-03-20 01:28:12 +0900

    [Fix] Modify Path of API

--- src/route/schedule_route.js
+++ src/route/schedule_route.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const Root = require('../config/root');
 const ScheduleController = require('../controller/schedule_controller');
 
-router.post('/create', Root.authentication, ScheduleController.create);
-router.put('/update', Root.authentication, ScheduleController.update);
+router.post('', Root.authentication, ScheduleController.create);
+router.put('', Root.authentication, ScheduleController.update);
 router.delete( '', Root.authentication, ScheduleController.delete);
 
 module.exports = router;
