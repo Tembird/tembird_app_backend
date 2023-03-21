6944ad9e1ae6fa1332238c62d7e63dee9046a203
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-03-21 22:00:16 +0900

    [Refactor] Change Path of SendEmailVerificationEmail API

--- src/route/verification_route.js
+++ src/route/verification_route.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const VerificationController = require('../controller/verification_controller');
 
-router.post('/require', VerificationController.sendEmailVerificationEmail);
+router.post('/send', VerificationController.sendEmailVerificationEmail);
 router.post('/check', VerificationController.checkEmailVerificationEmail);
 
 module.exports = router;
